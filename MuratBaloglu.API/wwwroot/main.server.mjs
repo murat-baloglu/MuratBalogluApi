f4c027b41a25c49e180aca930960e09c1360015f
murat-baloglu <op.dr.muratbaloglu@gmail.com>
2024-07-18 16:55:57 +0300

    Updated wwwroot folder

--- MuratBaloglu.API/wwwroot/main.server.mjs
+++ MuratBaloglu.API/wwwroot/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZPH5XDKW.mjs";import"./chunk-PPLZRD4B.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RFMKO43M.mjs";import"./chunk-PPLZRD4B.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
