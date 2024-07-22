7fc997d613794ef1ff2ba38be71adb9b3a0e95db
murat-baloglu <op.dr.muratbaloglu@gmail.com>
2024-07-22 03:17:32 +0300

    22.07.2024

--- MuratBaloglu.API/wwwroot/main.server.mjs
+++ MuratBaloglu.API/wwwroot/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RFMKO43M.mjs";import"./chunk-PPLZRD4B.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PQKT2A7P.mjs";import"./chunk-FBOHJEHE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
